70f9a83b4a2a84e970ff876a470dced5aa3347f9
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-07-02 18:59:28 -0300

    atualização da nav-bar

--- src/views/multas/Multas-lista-view.vue
+++ src/views/multas/Multas-lista-view.vue
@@ -1,37 +1,5 @@
 <template>
 
-<nav class=" navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid nav">
-    <img alt="Logo"  src="LogoNav.png" class="logo">
-
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Motoristas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Veiculos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Configurações</a>
-        </li>
-      </ul>
-
-
-      <div class="d-flex">
-            <p class="nomeUsuario">Pedro Henrique</p>
-           <div class="logoUsuario">
-            P
-           </div>
-      </div>
-      <div>
-        <button class="btn botao" type="button">
-            Sair
-            <img src="LogoSair.png">
-        </button>
-      </div>
-    </div>
-</nav>
-
 <div class="container tabela">
         <div class="container text-center">
             <div class="row align-items-start">
