1f689c1fa929e23aa16bf9973e07240325cd2db0
cris <cristovaom22@gmail.com>
2023-07-03 19:57:39 -0300

    feat(all): funcional

--- src/Components/Nav.vue
+++ src/Components/Nav.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="bg-green nav-size">
     <div class="d-flex justify-content-evenly h-100 d-inline-block">
-      <div class="h-100 d-inline-block d-flex align-items-end">
+      <div class="h-100 d-inline-block nav-sts d-flex align-items-end">
         <ul class="nav nav-tabs">
-      
-          <img alt="Vue logo" class="logo me-4" src="../assets/fleetControll.png">
+            <img src="">
+          <img alt="Vue logo" class="logo me-4 d-flex" src="../assets/fleetControll.png">
           <li class="nav-item mt-5">
             <a class="nav-link fw-bolder text-color" aria-current="page" href="/usuario-lista">Usuario</a>
           </li>
@@ -35,8 +35,8 @@
 
       <div class="d-flex align-items-center">
         <div class="d-flex flex-column">
-          <h8 class="text-color ">Matheus EL Marge</h8>
-          <h5>admin</h5>
+          <h8 class="text-color ">ADMIN</h8>
+          <h5 class="cor-adm">admin</h5>
         </div>
         <router-link to="/">
         <button type="button" class="btn btn-danger ms-5  text-color fw-bold fs-font">
@@ -64,6 +64,12 @@ export default defineComponent({
   width: 100vw;
   height: 100vh;
 }
+.cor-adm{
+  color: white;
+}
+.logo{
+  margin-top: 10%;
+}
 #config-button {
   background-color: #ff0000;
 }
@@ -86,6 +92,10 @@ export default defineComponent({
 a {
   text-decoration: none;
   transition: 1ms;
+}
+.nav-sts{
+  margin-top: 1%;
+  display: flex;
 }
 
 a :active{
